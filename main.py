6f4950e716aad481a156b76c394b3d6ff7163f38
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-27 23:25:16 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class filterDerifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = filterDerifyWorker()
+    print("Worker engine initialized.")
